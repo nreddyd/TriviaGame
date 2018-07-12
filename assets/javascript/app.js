1e72d3bcfdf34de34b574480df4d3932c2c721ef
Neeraja <neerajareddy24@gmail.com>
2018-07-12 03:10:22 -0400

    updated file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@ var trivia = [
   {
     question: "question1",
     options: ["Q1option1", "Q1option2", "Q1option3", "Q1option4"],
-    answer: "option1"
+    answer: "Q1option1"
   },
   {
     question: "question2",
@@ -26,34 +26,87 @@ var trivia = [
   }
 ];
 
-var count = 0;
+var game = {
+  count: 2,
+  triviaindex: 0,
+  currentanswer: "",
+  correctanswers: 0,
+  wronganswers: 0,
+  answered: 0,
+  timer: function() {
+    console.log(game.count);
+    game.count--;
+    $("#counter").text(game.count);
+    if (game.count === 0) {
+      game.timeup();
+    }
+  },
+  displayquestion: function() {
+    TriviaID = setInterval(this.timer, 2000);
+    $("#triviaquestion").text(trivia[this.triviaindex].question);
+    $("#option1label").text(trivia[this.triviaindex].options[0]);
+    $("#option2label").text(trivia[this.triviaindex].options[1]);
+    $("#option3label").text(trivia[this.triviaindex].options[2]);
+    $("#option4label").text(trivia[this.triviaindex].options[3]);
+  },
+  nextquestion: function() {
+    this.count = 2;
+    $("#counter").text(this.count);
+    this.triviaindex++;
+    this.displayquestion();
+  },
+  timeup: function() {
+    clearInterval(TriviaID);
+    $("#counter").text(game.count);
+    if (game.triviaindex < trivia.length - 1) {
+      game.nextquestion();
+    } else {
+      game.displayresult();
+    }
+  },
+  displayresult: function() {
+    $("#container").html("<p>correct answers " + this.correctanswers + "<p>");
+    $("#container").append("<p>wrong answer " + this.wronganswers + "<p>");
+    $("#container").append("<p>unanswerd " + (5 - this.answered) + "<p>");
+  },
+  clicked: function() {
+    clearInterval(TriviaID);
+    this.answered++;
 
+    $("#option1").prop("checked", false);
+    $("#option2").prop("checked", false);
+    $("#option3").prop("checked", false);
+    $("#option4").prop("checked", false);
+
+    console.log("inside clicked");
+    if (this.currentanswer === trivia[this.triviaindex].answer) {
+      this.correctanswers++;
+      if (this.triviaindex < trivia.length - 1) {
+        this.nextquestion();
+      } else {
+        this.displayresult();
+      }
+    } else {
+      this.wronganswers++;
+      if (this.triviaindex < trivia.length - 1) {
+        this.nextquestion();
+      } else {
+        this.displayresult();
+      }
+    }
+  }
+};
 $("#start").on("click", function() {
-  $("#triviaquestion").text(trivia[0].question);
-  $("#customCheck1label").text(trivia[0].options[0]);
-  $("#customCheck2label").text(trivia[0].options[1]);
-  $("#customCheck3label").text(trivia[0].options[2]);
-  $("#customCheck4label").text(trivia[0].options[3]);
+  game.displayquestion();
+});
+$(".answercheckbox").on("click", function() {
+  game.currentanswer = $(this)
+    .next("label")
+    .text();
+
+  game.clicked();
 });
 
-// function displayquestion() {
-//   if (count === trivia.length) {
-//     $("#questioncontainer").text("THe ENd");
-//   }
-
-//   for (i = o; i < trivia.length; i++) {
-//     $("#triviaquestiom").html(trivia[i].question);
-//     $("#customCheck1label").html(trivia[i].option[0]);
-//     $("#customCheck2label").html(trivia[i].option[1]);
-//     $("#customCheck3label").html(trivia[i].option[2]);
-//     $("#customCheck4label").html(trivia[i].option[3]);
-//   }
-
-//   count++;
-// }
-
-// function startquestions() {
-//   triviainterval = setInterval(displayquestion, 2000);
-// }
-
-// startquestions();
+// $('#type1').click(function() {
+//     $('#type2').not('#type1').removeAttr('checked');
+// });
